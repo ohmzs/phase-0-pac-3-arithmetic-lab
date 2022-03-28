1fa2d797702a769cd3d7f9733a7faafd5e5d4021
Omar <omartsadek@gmail.com>
2022-03-28 12:46:54 -0400

    commit tree 9b042beed998c1b95f6dd7febd025fecd7cd429e

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(a,b) {   
+    return a + b;
+}
+function subtract(a,b) {
+    return a - b;
+}
+function multiply(a,b) { 
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function increment(n){
+    return n+1;
+}
+function decrement(n){
+    return n-1;
+}
+let base = 10;
+function makeInt(n){
+    return n;
+}
+
+
+    
+
+
+
+
+function parses(n){
+    return n;
+}
+
